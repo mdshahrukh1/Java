7649573c1d66ee4f3f1fffddf397c9c00118202d
mdshahrukh1 <156064452+mdshahrukh1@users.noreply.github.com>
2025-04-13 21:58:50 +0530

    Update PrimeNumber.java
    different type of solution

--- 14_FunctionsAndMethods/PrimeNumber.java
+++ 14_FunctionsAndMethods/PrimeNumber.java
@@ -1,22 +1,34 @@
 public class PrimeNumber {
     // public static boolean isPrime(int n){
-    //     //corner Cases only for n >= 2
-    //     if(n == 2){
-    //         return true;
-    //     }
-
-    //     // boolean isPrime = true;
-
-    //     for(int i = 2; i <= n-1; i++){
-    //         if(n % i == 0){  //Completely dividing
-    //             // isPrime = false;
-    //             // break; //return isPrime;
-    //             return false;
-    //         }
-    //     }
-    //     // return isPrime;
-    //     return true;
+    // //corner Cases only for n >= 2
+    // if(n == 2){
+    // return true;
     // }
+
+    // // boolean isPrime = true;
+
+    // for(int i = 2; i <= n-1; i++){
+    // if(n % i == 0){ //Completely dividing
+    // // isPrime = false;
+    // // break; //return isPrime;
+    // return false;
+    // }
+    // }
+    // // return isPrime;
+    // return true;
+    // }
+    
+    // using factors
+    // int n = ... // some integer
+    // int factors = 0;for(
+    // int i = 1;i<=n;i++)
+    // {
+    //     if (n % i == 0) {
+    //         factors++;
+    //     }
+    //     if (factors > 2)
+    //         break; // more than two factors means not a prime
+    // }System.out.println(factors==2?"Prime":"Not Prime");
 
     // Optimized Solution
     public static boolean isPrime(int n){
